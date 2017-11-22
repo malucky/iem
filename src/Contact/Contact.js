8151ef4b4658ca4a0afb5693aceba0022a4efbfc
Travis <noreply@travis-ci.org>
2017-11-22 23:49:27 +0800

    Fix warnings

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -10,15 +10,15 @@ const Contact = () => {
       <div dir="ltr">Brooklyn ( 布碌崙)</div>
       <div dir="ltr">5609 8th Ave 2/F</div>
       <div dir="ltr">Brooklyn NY 11220</div>
-      <div dir="ltr">(Tel)&nbsp;<a href="tel:718-437-7717" target="_blank">718-437-7717</a></div>
+      <div dir="ltr">(Tel)&nbsp;<a href="tel:718-437-7717">718-437-7717</a></div>
       <div dir="ltr">&nbsp;</div>
       <div dir="ltr">Flushing (法拉盛)</div>
       <div dir="ltr">135-05 40th Road</div>
       <div dir="ltr">Flushing NY 11354&nbsp;</div>
-      <div dir="ltr">(Tel)&nbsp;<a href="tel:718-888-3313" target="_blank">718-888-3113</a></div>
+      <div dir="ltr">(Tel)&nbsp;<a href="tel:718-888-3313">718-888-3113</a></div>
       <div dir="ltr">&nbsp;</div>
-      <div dir="ltr">Email:&nbsp;<a href="mailto:info@iemshowplace.com" target="_blank">info@iemshowplace.com</a></div>
-      <div dir="ltr">Facebook:&nbsp;<a href="http://www.facebook.com/iemshowplace88" target="_blank">IEM Show Place</a></div>
+      <div dir="ltr">Email:&nbsp;<a href="mailto:info@iemshowplace.com">info@iemshowplace.com</a></div>
+      <div dir="ltr">Facebook:&nbsp;<a href="http://www.facebook.com/iemshowplace88">IEM Show Place</a></div>
     </Page>
   );
 };
