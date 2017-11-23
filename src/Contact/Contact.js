d854c20b9ade5fc68e13f088a6eda14498fae34b
Travis <noreply@travis-ci.org>
2017-11-23 13:43:03 +0800

    Updated terms and contacts makrup and style

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -5,20 +5,24 @@ import './Contact.css';
 const Contact = () => {
   return (
     <Page title="聯繫信息 Contact Us">
-      <p>We're happy to answer questions.<br />Give us a call from the numbers below.</p>
-      <p>&nbsp;我們會說中文.</p>
-      <div dir="ltr">Brooklyn ( 布碌崙)</div>
-      <div dir="ltr">5609 8th Ave 2/F</div>
-      <div dir="ltr">Brooklyn NY 11220</div>
-      <div dir="ltr">(Tel)&nbsp;<a href="tel:718-437-7717">718-437-7717</a></div>
-      <div dir="ltr">&nbsp;</div>
-      <div dir="ltr">Flushing (法拉盛)</div>
-      <div dir="ltr">135-05 40th Road</div>
-      <div dir="ltr">Flushing NY 11354&nbsp;</div>
-      <div dir="ltr">(Tel)&nbsp;<a href="tel:718-888-3313">718-888-3113</a></div>
-      <div dir="ltr">&nbsp;</div>
-      <div dir="ltr">Email:&nbsp;<a href="mailto:info@iemshowplace.com">info@iemshowplace.com</a></div>
-      <div dir="ltr">Facebook:&nbsp;<a href="http://www.facebook.com/iemshowplace88">IEM Show Place</a></div>
+      <section className="contact">
+        <p>We're happy to answer questions.<br />Give us a call from the numbers below.</p>
+        <p>我們會說中文.</p>
+        <address className="contact-address">
+          Brooklyn (布碌崙)<br />
+          5609 8th Ave 2/F<br />
+          Brooklyn NY 11220<br />
+          (Tel) <a href="tel:718-437-7717">718-437-7717</a><br />
+          <br />
+          Flushing (法拉盛)<br />
+          135-05 40th Rd<br />
+          Flushing NY 11354<br />
+          (Tel) <a href="tel:718-888-3313">718-888-3113</a><br />
+          <br />
+          Email: <a href="mailto:info@iemshowplace.com">info@iemshowplace.com</a><br />
+          Facebook: <a href="http://www.facebook.com/iemshowplace88">IEM Show Place</a><br />
+        </address>
+      </section>
     </Page>
   );
 };
--- src/Terms/Terms.js
+++ src/Terms/Terms.js
@@ -5,78 +5,57 @@ import './Terms.css';
 const Terms = () => {
   return (
       <Page title="購票須知 TERMS & CONDITIONS">
-        <p>購票前請詳閱注意事項：</p>
-        <p>1.IEMSHOWPLACE.COM網路購票對於無法挑選座位的區域採取先買先得方式，由您購</p>
-        <p>買價格電腦挑選的門票價格中間賣起至兩旁</p>
-        <p>2. 同一個訂購號碼的門票會安排坐在一起. 如分開購買, 但希望安排坐在一起. 請致電 718-</p>
-        <p>888-3113.</p>
-        <p>2. 購票時，請選擇取票三種方式：</p>
-        <p>(一) 郵寄 -演唱會門票將郵寄到訂票地址 Priority Mailing Service 及手續費： 美金15元，</p>
-        <p>票數不限。如果你的信用卡地址與郵寄地址不一樣的，請在備註中注明你的郵寄地址。</p>
-        <p>我們會以Priority Mail寄出。USPS 會發出查詢號碼去到你的電郵. (演唱會前7天購票者，</p>
-        <p>恕不接受郵件寄票）. 如果開演前7天仍未收到演唱會票，請致電 718-888- 3113與主辦聯</p>
-        <p>絡查詢，否則視做已經收到，現場不再辦理。</p>
-        <p>(二) 門市取票- 布碌崙: 5609 8大道 2樓 / 法拉盛: 135-05 40路 2樓. *請務必攜帶與購買者</p>
-        <p>相符之證件以茲證明</p>
-        <p>(三)演場取票-請於演唱會當天（開場前2小時至開場後20分鐘）前往現場取票處（Stage</p>
-        <p>pick up）取票。現場取票時請務必攜帶與購買者相符之證件以茲證明。</p>
-        <p>4. 美國境外訂票者，恕不接受郵寄，請於演出當天至演唱會現場取票。</p>
-        <p>5. 請注意：網上購票交易成功後，不可以換票，退票。</p>
-        <p>6.為維護您的權益，請勿向他人購買票券，屆時將無法進場，主辦單位保留取消該票之權</p>
-        <p>利。</p>
-        <p>7.請確實核對訂購內容，本票劵一經售出，表示台端同意支付本次交易的內容與價格，台</p>
-        <p>端不得以任何理由拒付本次交易費用。</p>
-        <p>8.入場卷請小心保管，如發生遺失、損毀、破損等情形，一律不予重新開票。任意塗改、</p>
-        <p>影印或套印、掃描複製票劵，均屬無效票。</p>
-        <p>9.一人一票，憑票入場，孩童亦需購票。因考量整體音量恐對孩童造成影響及其身高受限</p>
-        <p>而影響視線，購票前請自行斟酌。</p>
-        <p>10.活動現場嚴禁使用相機、攝影機；禁止攝錄影、錄音。如經查獲，將由工作人員留置</p>
-        <p>器材保管並刪除拍攝內容，但損壞與遺失概不負責，現場工作人員亦有權將底片與記憶卡</p>
-        <p>加以強制曝光或強制刪除，並保留法律追訴權，謝謝您的合作。</p>
-        <p>11. 場內禁止攜帶以下器材/物品，違者逕行沒收。 a. 自拍棒、自拍神器等器材。 b. 手拍</p>
-        <p>、哨子、加油棒等製造聲響之物品。c.大面積、LED燈牌類、氣球及板凳等會妨礙到其他</p>
-        <p>歌迷觀賞演唱會的應援製作物。</p>
-        <p>12. 場內禁帶外食飲料、寵物，敬請配合。開演後手機請關機或開靜音。</p>
-        <p>13. 活動若因故延期或取消，主辦單位不負交通及住宿費之補償。</p>
-        <p>14.相關規定以活動官網及現場公告為主，主辦單位保留加場、修改、終止及本活動相關</p>
-        <p>演出內容之權力。</p>
-        <p>15.請於購票前確認購票細節。並呼籲您不要購買來路不明的票劵，以免自身權益受損，</p>
-        <p>主辦單位將保留認定票券合法性之權力。其他購票相關問題請環球客服諮詢專線718-</p>
-        <p>888-3113（上班時間：週一～週日 8:00AM-7:00PM）由客服人員為您服務解說。</p>
-        <p>16. 通過iemshowplace.com網站訂購門票及一切服務您必須承認及同意上述條款和條件.</p>
-        <p>並請遵守演出場地的規定</p>
-        <p>有關門票的疑問，請致電 718-437- 7717 或718-888- 3113</p>
-        <p>Online Ticket Policy / Please Read Before Purchase</p>
-        <p>1. There is no service charge for online ticket purchase. Children need a full price ticket.&nbsp;</p>
-        <p>2. If you choose to receive your order through mail delivery as opposed to local pickup, please make sure</p>
-        <p>the purchase is made 7 business days prior to the concert.</p>
-        <p>3. Please select the mailing option when you buy tickets online: &nbsp;Priority Mailing Service and handling</p>
-        <p>fee： If you choose Priority Mailing ($15.00), we will mail the tickets to your registered address via US</p>
-        <p>Postal Services Priority Mail, meantime the tracking number will be sent to your email address by USPS.</p>
-        <p>Note:</p>
-        <p>1. If your mailing address is different from your billing address, please write down your mailing address</p>
-        <p>in the Mailing address box.</p>
-        <p>2. If you haven&rsquo;t received your tickets 7 days before the concert, please call us immediately 718-888- 3113</p>
-        <p>to check the status. We will NOT be able to assist you on event day if you do not call in advance.</p>
-        <p>3. Put your tickets in a safe place. We can&rsquo;t help you if the tickets are damaged or lost.</p>
-        <p>4. If you choose Local Pick up service, you should pick up your tickets 2 hours before to 20 minutes after</p>
-        <p>the concert begins at the concert venue entrance. Please make sure to bring your photo ID and credit card</p>
-        <p>used on your order.</p>
-        <p>5. Seats will be assigned starting from center column toward side columns on first come, first service</p>
-        <p>basis for online purchasers. General admission ticket price zone does not offer seat selection option.</p>
-        <p>6. Ticket mailing service is not offered to purchasers with mailing address outside of Continental United</p>
-        <p>States. Please use Stage Pickup service.</p>
-        <p>7. Please be noted that there is no refund or exchange once the online purchase is completed.</p>
-        <p>8. Orders are subject to credit card approval and processed only after a billing address, and other billing</p>
-        <p>information, has been verified. Occasionally, we receive incorrect billing or credit card account</p>
-        <p>information for a ticket order that can delay processing and delivery. In these cases, IEMshowplace</p>
-        <p>customer service will attempt to contact you, using the information provided at the time of purchase. If</p>
-        <p>we are unable to reach you after our initial attempt, we may cancel your order and may sell your tickets</p>
-        <p>to another customer without further notice.</p>
-        <p>8.Ticket purchasers shall observe the rules set by the Box Office of the Venue too.&nbsp;</p>
-        <p>9. IEMshowplace.com reserves all rights.</p>
-        <p>By purchased though our website, you agree to our above term of use.</p>
-        <p>Any further question regarding ticket purchase, please call 718-437- 7717 or 718-888- 3113.</p>
+        <section className="terms">
+          <p>購票前請詳閱注意事項：</p>
+          <ol>
+            <li>IEMSHOWPLACE.COM網路購票對於無法挑選座位的區域採取先買先得方式，由您購買價格電腦挑選的門票價格中間賣起至兩旁</li>
+            <li>同一個訂購號碼的門票會安排坐在一起. 如分開購買, 但希望安排坐在一起. 請致電 <a href="tel:718-888-3113">718-888-3113</a>.</li>
+            <li>
+              購票時，請選擇取票三種方式
+              <ol>
+                <li>郵寄 -演唱會門票將郵寄到訂票地址 Priority Mailing Service 及手續費： 美金15元，票數不限。如果你的信用卡地址與郵寄地址不一樣的，請在備註中注明你的郵寄地址。我們會以Priority Mail寄出。USPS 會發出查詢號碼去到你的電郵. (演唱會前7天購票者，恕不接受郵件寄票）. 如果開演前7天仍未收到演唱會票，請致電 <a href="tel:718-888-3113">718-888-3113</a> 與主辦聯絡查詢，否則視做已經收到，現場不再辦理。</li>
+                <li>門市取票- 布碌崙: 5609 8大道 2樓 / 法拉盛: 135-05 40路 2樓. *請務必攜帶與購買者相符之證件以茲證明</li>
+                <li>演場取票-請於演唱會當天（開場前2小時至開場後20分鐘）前往現場取票處（Stage pick up）取票。現場取票時請務必攜帶與購買者相符之證件以茲證明。</li>
+              </ol>
+            </li>
+            <li>美國境外訂票者，恕不接受郵寄，請於演出當天至演唱會現場取票。</li>
+            <li>請注意：網上購票交易成功後，不可以換票，退票。</li>
+            <li>為維護您的權益，請勿向他人購買票券，屆時將無法進場，主辦單位保留取消該票之權利。</li>
+            <li>請確實核對訂購內容，本票劵一經售出，表示台端同意支付本次交易的內容與價格，台端不得以任何理由拒付本次交易費用。</li>
+            <li>入場卷請小心保管，如發生遺失、損毀、破損等情形，一律不予重新開票。任意塗改、影印或套印、掃描複製票劵，均屬無效票。</li>
+            <li>一人一票，憑票入場，孩童亦需購票。因考量整體音量恐對孩童造成影響及其身高受限而影響視線，購票前請自行斟酌。</li>
+            <li>活動現場嚴禁使用相機、攝影機；禁止攝錄影、錄音。如經查獲，將由工作人員留置器材保管並刪除拍攝內容，但損壞與遺失概不負責，現場工作人員亦有權將底片與記憶卡加以強制曝光或強制刪除，並保留法律追訴權，謝謝您的合作。</li>
+            <li>場內禁止攜帶以下器材/物品，違者逕行沒收。 a. 自拍棒、自拍神器等器材。 b. 手拍、哨子、加油棒等製造聲響之物品。c.大面積、LED燈牌類、氣球及板凳等會妨礙到其他歌迷觀賞演唱會的應援製作物。</li>
+            <li>場內禁帶外食飲料、寵物，敬請配合。開演後手機請關機或開靜音。</li>
+            <li>活動若因故延期或取消，主辦單位不負交通及住宿費之補償。</li>
+            <li>相關規定以活動官網及現場公告為主，主辦單位保留加場、修改、終止及本活動相關演出內容之權力。</li>
+            <li>請於購票前確認購票細節。並呼籲您不要購買來路不明的票劵，以免自身權益受損，主辦單位將保留認定票券合法性之權力。其他購票相關問題請環球客服諮詢專線 <a href="tel:718-888-3113">718-888-3113</a>（上班時間：週一～週日 8:00AM-7:00PM）由客服人員為您服務解說。</li>
+            <li>通過iemshowplace.com網站訂購門票及一切服務您必須承認及同意上述條款和條件.</li>
+          </ol>
+          <p>並請遵守演出場地的規定</p>
+          <p>有關門票的疑問，請致電 <a href="tel:718-437-7717">718-437-7717</a> 或 <a href="tel:718-888-3113">718-888-3113</a></p>
+
+          <p>Online Ticket Policy / Please Read Before Purchase</p>
+          <ol>
+            <li>There is no service charge for online ticket purchase. Children need a full price ticket.</li>
+            <li>If you choose to receive your order through mail delivery as opposed to local pickup, please make sure the purchase is made 7 business days prior to the concert.</li>
+            <li>Please select the mailing option when you buy tickets online:
+              <ol>
+                <li>Priority Mailing Service and handling fee：If you choose Priority Mailing ($15.00), we will mail the tickets to your registered address via US Postal Services Priority Mail, meantime the tracking number will be sent to your email address by USPS. Note: If your mailing address is different from your billing address, please write down your mailing address in the Mailing address box. If you haven&rsquo;t received your tickets 7 days before the concert, please call us immediately <a href="tel:718-888-3113">718-888-3113</a> to check the status. We will NOT be able to assist you on event day if you do not call in advance. Put your tickets in a safe place. We can&rsquo;t help you if the tickets are damaged or lost.</li>
+                <li>Purchase in store: You can purchase in person prior to the concert at 5609 8th Ave 2/F Brooklyn NY or 135-05 40th Rd Flushing NY.</li>
+                <li>Local Pickup service: You should pick up your tickets 2 hours before to 20 minutes after the concert begins at the concert venue entrance. Please make sure to bring your photo ID and credit card used on your order.</li>
+              </ol>
+            </li>
+            <li>Seats will be assigned starting from center column toward side columns on first come, first service basis for online purchasers. General admission ticket price zone does not offer seat selection option.</li>
+            <li>Ticket mailing service is not offered to purchasers with mailing address outside of Continental United States. Please use Stage Pickup service.</li>
+            <li>Please be noted that there is no refund or exchange once the online purchase is completed.</li>
+            <li>Orders are subject to credit card approval and processed only after a billing address, and other billing information, has been verified. Occasionally, we receive incorrect billing or credit card account information for a ticket order that can delay processing and delivery. In these cases, IEMshowplace customer service will attempt to contact you, using the information provided at the time of purchase. If we are unable to reach you after our initial attempt, we may cancel your order and may sell your tickets to another customer without further notice.</li>
+            <li>Ticket purchasers shall observe the rules set by the Box Office of the Venue too.</li>
+            <li>IEMshowplace.com reserves all rights.</li>
+          </ol>
+          <p>By purchased though our website, you agree to our above term of use.</p>
+          <p>Any further question regarding ticket purchase, please call <a href="tel:718-437-7717">718-437-7717</a> or <a href="tel:718-888-3113">718-888-3113</a>.</p>
+        </section>
       </Page>
   );
 };
